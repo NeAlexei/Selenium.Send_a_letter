f89ae8f1420fcd13df85f814c7c05d9bc8a993f2
NeAlexei <a.nechitaylo@hypervsn.com>
2019-04-16 17:30:42 +0300

    Send a letter

--- src/test/java/TestPost.java
+++ src/test/java/TestPost.java
@@ -2,19 +2,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.openqa.selenium.edge.EdgeDriver;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
-import static org.testng.Assert.assertTrue;
 
 public class TestPost {
     WebDriver driver;
